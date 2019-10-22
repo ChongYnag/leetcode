124659d530e5747e650968cb7656fd197a9a71b1
chongyang <89305141@qq.com>
2019-10-22 20:27:37 +0800

    fix

--- test/string/lesson2.test.js
+++ test/string/lesson2.test.js
@@ -3,3 +3,7 @@ import subStr from '../../code/string/lesson2';
 test('subStr(00110011)',()=>{
   expect(subStr('00110011')).toEqual(['0011', '01', '1100', '10', '0011', '01'])
 })
+
+test('subStr(10101)',()=>{
+  expect(subStr('10101')).toEqual(['10', '01', '10', '01'])
+})
